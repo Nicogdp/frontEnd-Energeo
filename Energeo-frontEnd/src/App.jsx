04e5bd9a1e26a1a4df3d4adad641e5f4967790d2
Nicogdp <gusdanipe200220@gmail.com>
2025-05-26 09:23:55 -0300

    pusimos en mayusculas el ./Roter/AppRouter

--- Energeo-frontEnd/src/App.jsx
+++ Energeo-frontEnd/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { AppRouter } from './Router/appRouter';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/App.css'
 import { AppNavbar } from './components/Navbar';
 import { Footer } from './components/Footer';
+import { AppRouter } from './Router/AppRouter';
 
 
 function App() {
